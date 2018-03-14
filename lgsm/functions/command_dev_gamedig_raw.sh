1adb4216f519bbcacd5a2dc55255ce29447db52e
Daniel Gibbs <me@danielgibbs.co.uk>
2018-03-14 17:22:02 +0000

    Updated gamedig-raw

--- lgsm/functions/command_dev_gamedig_raw.sh
+++ lgsm/functions/command_dev_gamedig_raw.sh
@@ -6,6 +6,20 @@
 
 info_config.sh
 
+if [ "${engine}" == "unreal" ]||[ "${engine}" == "unreal2" ]; then
+	port=$((port + 1))
+elif [ "${engine}" == "realvirtuality" ]; then
+	port=$((port + 1))
+elif [ "${engine}" == "spark" ]; then
+	port=$((port + 1))
+elif [ "${engine}" == "idtech3_ql" ]; then
+	engine="quakelive"
+fi
+
+if [ -n "${queryport}" ]; then
+	port="${queryport}"
+fi
+
 local engine_query_array=( avalanche3.0 madness quakelive realvirtuality refractor source goldsource spark starbound unity3d )
 for engine_query in "${engine_query_array[@]}"
 do
@@ -22,5 +36,20 @@ do
 	fi
 done
 
+local engine_query_array=( idtech2 iw2.0 )
+for engine_query in "${engine_query_array[@]}"
+do
+	if [ "${engine_query}" == "${engine}" ]; then
+		gamedigengine="protocol-quake2"
+	fi
+done
+
+local engine_query_array=( idtech3 quake iw3.0 )
+for engine_query in "${engine_query_array[@]}"
+do
+	if [ "${engine_query}" == "${engine}" ]; then
+		gamedigengine="protocol-quake3"
+	fi
+done
 
 gamedig --type ${gamedigengine} --host ${ip} --port ${port}
